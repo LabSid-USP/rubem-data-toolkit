c605dcaf212416224a4999dcf3d15bba38a952b9
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2024-04-10 15:19:39 -0300

    Add initial ndvi ee app

---
+++ dados_serie_temporal/ndvi/ndvi_eeapp.js
@@ -0,0 +1,74 @@
+// Criar painéis para entrada de dados
+var panelMain = ui.Panel({
+    'layout': ui.Panel.Layout.flow('vertical'),
+    'style': {
+        'width': '360px',
+        'position': 'bottom-left',
+        'margin': '0px 0px 0px 0px',
+    },
+});
+
+var datePanel = ui.Panel();
+var collectionPanel = ui.Panel();
+var shapefilePanel = ui.Panel();
+
+// Adicionar widgets para entrada de dados
+datePanel.add(ui.Label('Digite a data inicial:'));
+datePanel.add(ui.Textbox());
+datePanel.add(ui.Label('Digite a data final:'));
+datePanel.add(ui.Textbox());
+
+collectionPanel.add(ui.Label('Selecione a coleção:'));
+collectionPanel.add(ui.Select({items: ['MODIS/006/MOD13A1', 'LANDSAT/LC08/C01/T1']}));
+
+// Adicionar painéis à interface do usuário
+panelMain.add(datePanel);
+panelMain.add(collectionPanel);
+panelMain.add(shapefilePanel);
+
+// Definir a função de download
+var downloadTasks = function() {
+  // Obter os valores de entrada
+  var startDate = datePanel.widgets().get(1).getValue();
+  var endDate = datePanel.widgets().get(3).getValue();
+  var collectionName = collectionPanel.widgets().get(1).getValue();
+  // var shapefilePath = shapefilePanel.widgets().get(1).getValue();
+
+  // TODO: Encontrar uma forma de importar um shapefile de geometria 
+  // https://developers.google.com/earth-engine/guides/table_upload#upload-a-shapefile
+  // Carregar o shapefile
+  var aoi = ee.FeatureCollection(shapefilePath);
+
+  // Selecionar o conjunto de dados
+  var dataset = ee.ImageCollection(collectionName).select('NDVI');
+
+  // Filtrar o conjunto de dados
+  var filtered = dataset.filterDate(startDate, endDate).filterBounds(aoi);
+
+  // Criar tarefas de download
+  filtered.map(function(image) {
+    // Obter a data da imagem
+    var date = image.date().format('yyyy-MM-dd');
+
+    // Definir as opções de exportação
+    var exportOptions = {
+      image: image,
+      description: 'NDVI_' + date,
+      scale: 500,
+      region: aoi,
+      folder: 'NDVI',
+      fileFormat: 'GeoTIFF'
+    };
+
+    // Criar a tarefa de exportação
+    var task = ee.batch.Export.image.toDrive(exportOptions);
+
+    // Iniciar a tarefa
+    task.start();
+  });
+};
+
+// Adicionar um botão para iniciar o download
+panelMain.add(ui.Button('Iniciar download', downloadTasks));
+
+ui.root.add(panelMain);
