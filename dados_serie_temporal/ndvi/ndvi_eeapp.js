1b0b2b3bd080091b8f6ec5ffb9540a68ad7c26bb
gabriel <gabriel@labsid.eng.br>
2024-05-07 14:21:04 +0000

    Updated dados_serie_temporal/ndvi/ndvi_eeapp.js (added 39 lines, deleted 39 lines).

--- dados_serie_temporal/ndvi/ndvi_eeapp.js
+++ dados_serie_temporal/ndvi/ndvi_eeapp.js
@@ -49,54 +49,54 @@ Map.addLayer(geometry, {}, 'Geometry')
 // NDVI visualization
 var palette = palettesGeneral.colorbrewer.RdYlGn[11];
 var ndviVis = {
-  min: 0.0,
+  min: -0.2,
   max: 1.0,
   palette: palette,
 };
 
 // Add bar Legend
 function createColorBar(titleText, palette, min, max) {
-// Legend Title
-var title = ui.Label({
-  value: titleText, 
-  style: {
-    fontWeight: 'bold', 
-    textAlign: 'center', 
-    stretch: 'horizontal'
-  }
-});
-
-// Colorbar
-var legend = ui.Thumbnail({
-  image: ee.Image.pixelLonLat().select(0),
-  params: {
-    bbox: [0, 0, 1, 0.1],
-    dimensions: '200x20',
-    format: 'png', 
-    min: 0, max: 1,
-    palette: palette
-  },
-  style: {stretch: 'horizontal', margin: '8px 8px', maxHeight: '40px'},
-});
+  // Legend Title
+  var title = ui.Label({
+    value: titleText, 
+    style: {
+      fontWeight: 'bold', 
+      textAlign: 'center', 
+      stretch: 'horizontal'
+    }
+  });
   
-// Legend Labels
-var labels = ui.Panel({
-  widgets: [
-    ui.Label(min, {margin: '4px 10px',textAlign: 'left', stretch: 'horizontal'}),
-    ui.Label((min+max)/2, {margin: '4px 20px', textAlign: 'center', stretch: 'horizontal'}),
-    ui.Label(max, {margin: '4px 10px',textAlign: 'right', stretch: 'horizontal'})],
-  layout: ui.Panel.Layout.flow('horizontal')});
-  
-// Create a panel with all 3 widgets
-var legendPanel = ui.Panel({
-  widgets: [title, legend, labels],
-  style: {position: 'bottom-center', padding: '8px 15px'}
-})
-return legendPanel
+  // Colorbar
+  var legend = ui.Thumbnail({
+    image: ee.Image.pixelLonLat().select(0),
+    params: {
+      bbox: [0, 0, 1, 0.1],
+      dimensions: '200x20',
+      format: 'png', 
+      min: -0.2, max: 1,
+      palette: palette
+    },
+    style: {stretch: 'horizontal', margin: '8px 8px', maxHeight: '40px'},
+  });
+    
+  // Legend Labels
+  var labels = ui.Panel({
+    widgets: [
+      ui.Label(min, {margin: '4px 10px',textAlign: 'left', stretch: 'horizontal'}),
+      ui.Label((min+max)/2, {margin: '4px 20px', textAlign: 'center', stretch: 'horizontal'}),
+      ui.Label(max, {margin: '4px 10px',textAlign: 'right', stretch: 'horizontal'})],
+    layout: ui.Panel.Layout.flow('horizontal')});
+    
+  // Create a panel with all 3 widgets
+  var legendPanel = ui.Panel({
+    widgets: [title, legend, labels],
+    style: {position: 'bottom-center', padding: '8px 15px'}
+  })
+  return legendPanel
 }
 
 // Call the function to create a colorbar legend  
-var colorBar = createColorBar('NDVI', palette, 0, 1)
+var colorBar = createColorBar('NDVI', palette, -0.2, 1)
 
 Map.add(colorBar)
 
