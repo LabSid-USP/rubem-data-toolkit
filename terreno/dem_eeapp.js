2caa5fdbc3b16d13a4ec9d21af02e23fb3f56e85
vichete <vichete@gmail.com>
2024-07-03 17:32:01 +0000

    Updated terreno/dem_eeapp.js (added 3 lines).

--- terreno/dem_eeapp.js
+++ terreno/dem_eeapp.js
@@ -13,6 +13,9 @@ var geometry =
           [-104.16484375, 40.43415242959552]]], null, false);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // RUBEM ToolKit dataset input process
+// 
+// Digital Terrain Elevation (DEM)
+//
 // Distributed hydrological model
 // https://doi.org/10.3390/w14121958
 // Documentation and tutorials: 
